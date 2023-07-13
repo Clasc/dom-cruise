356e17804cf9f402872428b05ed4ab7aa541a6ae
Christian Lascsak <christian.lascsak7@gmail.com>
2023-07-13 09:24:06 +0200

    carousel prototype

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Carousel from './carousel';
 
 function App() {
+  const els = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Carousel>
+          {els.map(e=><h2 key={e}>{e}</h2>)}
+        </Carousel>
       </header>
     </div>
   );
