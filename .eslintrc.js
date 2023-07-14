419c03b3b132089f3a6904658ac58bf579885852
Christian Lascsak <christian.lascsak7@gmail.com>
2023-07-14 08:19:59 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+    'plugin:react/recommended',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'max-len': 120,
+  },
+};
--- src/carousel/index.jsx
+++ src/carousel/index.jsx
@@ -1,41 +1,46 @@
-import { useEffect, useRef, useState } from "react";
-import "./index.css"
-const Carousel = ({ children }) => {
-    const [pos, setPos] = useState({ x: null, y: null, left: null, top: null });
-    const carouselRef = useRef(null);
-    const [isGrabbing, setIsGrabbing] = useState(false);
-    const mouseUpHandler = () => setIsGrabbing(false);
+import {useRef, useState} from 'react';
+import './index.css';
+const Carousel = ({children}) => {
+  const [pos, setPos] = useState({x: null, y: null, left: null, top: null});
+  const carouselRef = useRef(null);
+  const [isGrabbing, setIsGrabbing] = useState(false);
+  const mouseUpHandler = () => setIsGrabbing(false);
 
-    const mouseDownHandler = (e) => {
-        setPos({
-            // The current scroll
-            left: carouselRef.current.scrollLeft,
-            top: carouselRef.current.scrollTop,
-            // Get the current mouse position
-            x: e.clientX,
-            y: e.clientY,
-        });
-        setIsGrabbing(true);
-    };
+  const mouseDownHandler = (e) => {
+    setPos({
+      // The current scroll
+      left: carouselRef.current.scrollLeft,
+      top: carouselRef.current.scrollTop,
+      // Get the current mouse position
+      x: e.clientX,
+      y: e.clientY,
+    });
+    setIsGrabbing(true);
+  };
 
-    const mouseMoveHandler = function (e) {
-        if(!isGrabbing) return;
-        // How far the mouse has been moved
-        const dx = e.clientX - pos.x;
-        const dy = e.clientY - pos.y;
-        console.log(carouselRef.current);
-        // Scroll the element
-        carouselRef.current.scrollTop = pos.top - dy;
-        carouselRef.current.scrollLeft = pos.left - dx;
-    };
+  const mouseMoveHandler = function(e) {
+    if (!isGrabbing) return;
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+    console.log(carouselRef.current);
+    // Scroll the element
+    carouselRef.current.scrollTop = pos.top - dy;
+    carouselRef.current.scrollLeft = pos.left - dx;
+  };
 
-    return (
-        <div>
-            <ul onMouseDown={mouseDownHandler} onMouseUp={mouseUpHandler} ref={carouselRef} onMouseMove={mouseMoveHandler} className="carousel" style={{ cursor: isGrabbing && "grab", scrollSnapType:isGrabbing && "none" }}>
-                {children.map(c => <li className="carousel-element">{c}</li>)}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul
+        onMouseDown={mouseDownHandler} onMouseUp={mouseUpHandler}
+        ref={carouselRef}
+        onMouseMove={mouseMoveHandler}
+        className="carousel"
+        style={{cursor: isGrabbing && 'grab', scrollSnapType: isGrabbing && 'none'}}>
+        {children.map((c) => <li className="carousel-element">{c}</li>)}
+      </ul>
+    </div>
+  );
 };
 
 export default Carousel;
