1f6d60159addd64d6f1514eeb991d4de4cd0a66b
Christian Lascsak <christian.lascsak7@gmail.com>
2023-07-14 09:22:43 +0200

    improve width integration in app

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Carousel left={<button>Left</button>} right={<button>Right</button>} distance={124} >
-          {els.map((e)=><a href='www.google.com' key={e}>{e}</a>)}
-        </Carousel>
+        <div className='container'>
+          <Carousel left={<button>Left</button>} right={<button>Right</button>} distance={124} >
+            {els.map((e)=><a href='www.google.com' key={e}>{e}</a>)}
+          </Carousel>
+        </div>
       </header>
     </div>
   );
