df8d2c79d0f73ab90823938d696d9f6df4bf445f
Christian Lascsak <christian.lascsak7@gmail.com>
2023-09-19 08:58:12 +0200

    add element with alternate width and height

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Carousel from './carousel';
 
 // eslint-disable-next-line require-jsdoc
 function App() {
-  const els = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const els = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <div className="App">
       <header className="App-header">
         <div className='container'>
-          <Carousel distance={124} noScrollBar >
+          <Carousel distance={200} noScrollBar >
+            <div style={{ minWidth: 400, display: 'block',
+              height: 300, background: 'orange' }}>Test element</div>
             {els.map((e)=> <a href="www.google.com" key={e}>
-              <img src='https://picsum.photos/200'/>
+              <img src={`https://picsum.photos/seed/${e}/200`}/>
             </a>)}
+
           </Carousel>
         </div>
       </header>
