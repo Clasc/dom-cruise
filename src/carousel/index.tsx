fb570108aa80be0ef39bb9625eac5df184ea125a
Christian Lascsak <christian.lascsak7@gmail.com>
2023-09-19 08:22:33 +0200

    small refactorings

--- src/carousel/index.tsx
+++ src/carousel/index.tsx
@@ -1,11 +1,9 @@
 import React, { MouseEventHandler, useMemo, useRef, useState } from 'react';
 import './index.css';
+
 const classNames = (...classes: string[]) => classes.join(' ');
-
-
 type StyleProp = { [key: string]: string | number };
 
-// makeCssVariables takes an object and returns the same object but with the keys prefixed with '--'
 const makeCssVariables = <TVars extends StyleProp>(style: TVars) => {
   const result: StyleProp = {};
   for (const key in style) {
@@ -59,17 +57,22 @@ const Carousel = ({
     document.addEventListener('mouseup', mouseUpHandler);
   };
 
+  const scrollBy=({ x, y }:{ x: number, y: number })=>{
+    if (!carouselRef.current) return;
+    carouselRef.current.scrollTop = pos.top - y;
+    carouselRef.current.scrollLeft = pos.left - x;
+  };
 
   const mouseMoveHandler: MouseEventHandler<HTMLUListElement> = (e) => {
-    if (!isGrabbing || !carouselRef.current) return;
+    if (!isGrabbing) return;
     e.preventDefault();
-    // How far the mouse has been moved
-    const dx = e.clientX - pos.x;
-    const dy = e.clientY - pos.y;
 
-    // Scroll the element
-    carouselRef.current.scrollTop = pos.top - dy;
-    carouselRef.current.scrollLeft = pos.left - dx;
+    const mouseMoveDistance = {
+      x: e.clientX - pos.x,
+      y: e.clientY - pos.y,
+    };
+
+    scrollBy(mouseMoveDistance);
   };
 
   const moveNext = () => {
