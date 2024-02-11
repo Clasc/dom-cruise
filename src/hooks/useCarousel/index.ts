0c31418993754e475ad10dcb3bb1af737906758a
Christian Lascsak <christian.lascsak7@gmail.com>
2024-02-11 13:54:42 +0100

    format

--- src/hooks/useCarousel/index.ts
+++ src/hooks/useCarousel/index.ts
@@ -1,28 +1,28 @@
 import { useMemo, useRef, useState } from 'react';
 
-export const useCarousel=()=>{
+export const useCarousel = () => {
   const carouselRef = useRef<HTMLUListElement>(null);
 
 
-  const slides = useMemo(()=>{
+  const slides = useMemo(() => {
     if (!carouselRef.current) return null;
     return carouselRef.current.children;
   }, [carouselRef.current?.innerHTML]);
 
-  const [scrollPos, setScrollPos] =useState(0);
-  const wholeWidth = carouselRef.current?.scrollWidth??0;
-  const currentSlideWith = useMemo(()=>{
+  const [scrollPos, setScrollPos] = useState(0);
+  const wholeWidth = carouselRef.current?.scrollWidth ?? 0;
+  const currentSlideWith = useMemo(() => {
     if (!slides) return 0;
-    const centeredSlide = Math.round(wholeWidth/scrollPos);
-    return (slides?.item(centeredSlide)as HTMLElement)?.offsetWidth ?? 0;
+    const centeredSlide = Math.round(wholeWidth / scrollPos);
+    return (slides?.item(centeredSlide) as HTMLElement)?.offsetWidth ?? 0;
   }, [carouselRef.current?.innerHTML]);
 
-  const currentScroll = ()=>({
+  const currentScroll = () => ({
     left: carouselRef.current?.scrollLeft ?? 0,
     top: carouselRef.current?.scrollTop ?? 0,
   });
 
-  const scrollBy=({ x, y }:{ x: number, y: number }, pos:{top:number, left:number})=>{
+  const scrollBy = ({ x, y }: { x: number, y: number }, pos: { top: number, left: number }) => {
     if (!carouselRef.current) return;
     carouselRef.current.scrollTop = pos.top - y;
     carouselRef.current.scrollLeft = pos.left - x;
