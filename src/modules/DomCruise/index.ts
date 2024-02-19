c21660263d27972fc3df7bb520929504fc9423d8
Christian Lascsak <christian.lascsak7@gmail.com>
2024-02-19 12:26:38 +0100

    fix sdtuff

--- src/modules/DomCruise/index.ts
+++ src/modules/DomCruise/index.ts
@@ -1,31 +1,33 @@
-/* eslint-disable require-jsdoc */
-
 import dragToScroll from "../dragToScroll";
 import state from "../state";
 
 
-export const DomCruise = (carousel: HTMLUListElement, { prev, next }: { prev: HTMLElement, next: HTMLElement }) => {
-  const slides = carousel.children;
-  const [scrollPos, setScrollPos] = state(0);
+export const DomCruise = (carousel: HTMLUListElement, options: { prev: HTMLElement, next: HTMLElement }) => {
+  const { prev, next } = options;
 
-  const wholeWidth = carousel.scrollWidth ?? 0;
-  const currentSlideWith = () => {
-    const centeredSlide = Math.round(wholeWidth / scrollPos.ref);
-    return (slides?.item(centeredSlide) as HTMLElement)?.offsetWidth ?? 0
+  const slides = carousel.children;
+  const [currentScroll, setCurrentScroll] = state(0);
+
+  const currentSlideWidth = () => {
+    const carouselRect = carousel.getBoundingClientRect();
+    const slideOffsets = Array.from(slides).map(s => {
+      return Math.abs((carouselRect.x - s.getBoundingClientRect().x) + currentScroll.ref);
+    });
+    const index = slideOffsets.indexOf(Math.min(...slideOffsets));
+    return slides.item(index)?.clientWidth ?? 0;
   };
 
+  carousel.addEventListener('scrollend', (e) => {
+    setCurrentScroll((e.currentTarget as any)?.scrollLeft ?? 0);
+  });
+
   next.addEventListener('click', () => {
-    carousel.scrollLeft += currentSlideWith();
+    carousel.scrollLeft += currentSlideWidth();
   });
 
   prev.addEventListener('click', () => {
-    carousel.scrollLeft -= currentSlideWith();
+    carousel.scrollLeft -= currentSlideWidth();
   });
-
-  carousel.addEventListener('scroll', (e) => {
-    setScrollPos((e.currentTarget as any)?.scrollLeft ?? 0)
-  });
-
 
   dragToScroll({
     el: carousel,
